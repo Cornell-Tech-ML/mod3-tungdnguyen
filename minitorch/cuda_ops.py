2e66f2f6b5955daf4435100d0c2e9c85b9729c15
Tung Doan Nguyen <tnguye48@hawk.iit.edu>
2024-11-17 18:16:25 -0500

    crayz

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -352,7 +352,7 @@ def tensor_reduce(
             # If thread position is within the reduction dimension size
             # Loads the entire data of the reduced dimension into the shared memory.
             if pos < a_shape[reduce_dim]:
-                to_index(out_pos, out_shape, out_index)
+                to_index(i, out_shape, out_index)
                 out_index[reduce_dim] = pos
                 a_pos = index_to_position(out_index, a_strides)
                 cache[pos] = a_storage[a_pos]
@@ -361,12 +361,12 @@ def tensor_reduce(
             cuda.syncthreads()
 
             # Reduction in shared memory
-            stride = 2
-            while stride <= BLOCK_DIM//2:
-                if pos % stride == 0 and (pos + stride//2) < BLOCK_DIM:
-                    cache[pos] = fn(cache[pos],cache[pos + stride//2])
+            stride = BLOCK_DIM // 2
+            while stride > 0:
+                if pos < stride and pos + stride < a_shape[reduce_dim]:
+                    cache[pos] = fn(cache[pos], cache[pos + stride])
                 cuda.syncthreads()
-                stride *= 2
+                stride //= 2
             # Write result
             if pos == 0:
                 out[out_pos] = cache[0]
