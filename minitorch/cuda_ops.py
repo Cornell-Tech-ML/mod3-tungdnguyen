5341bbbf0d5eb70be9142afc86a145313bd53eb2
Tung Doan Nguyen <tnguye48@hawk.iit.edu>
2024-11-17 20:11:47 -0500

    hmm?

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -365,7 +365,8 @@ def tensor_reduce(
 
         # Write result
         if pos == 0:
-            out[out_pos] = cache[0]
+            new_out_pos = index_to_position(out_index, out_strides)
+            out[new_out_pos] = cache[0]
 
     return jit(_reduce)  # type: ignore
 
